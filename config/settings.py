4a56054f2187d70ec196d2043106c9238c606ca3
Kerbstomp <jpkerber92@gmail.com>
2023-02-01 17:32:46 -0600

    removed unused import

--- config/settings.py
+++ config/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-from secrets import token_hex
+# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-_o5bj$d(i76ymlb69dcj9p*hwko(tld0ffz1xv#-uxc4$a#+9o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
